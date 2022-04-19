9d705871e68e29ed82ca65df1cb483aa586f3891
Milosz Bargiel <miloszbargiel@gmail.com>
2022-04-19 12:27:40 +0200

    Add server config files and range slider plugin js map

--- src/js/settings.js
+++ src/js/settings.js
@@ -133,7 +133,7 @@ export const settings = {
     addressLengthMin: 5,
   },
   db: {
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     products: 'products',
     orders: 'orders',
     events: 'events',
